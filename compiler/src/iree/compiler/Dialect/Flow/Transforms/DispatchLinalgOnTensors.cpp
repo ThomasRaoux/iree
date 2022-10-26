c9598df74a64608e271a2622d97ca990c023063b
Thomas Raoux <thomasraoux@google.com>
2022-10-26 05:02:53 +0000

    add more restrictions

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/DispatchLinalgOnTensors.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/DispatchLinalgOnTensors.cpp
@@ -981,7 +981,7 @@ static bool isFusableWithProducer(OpOperand &operand, bool aggressiveFusion) {
       SmallVector<unsigned> dims;
       linalgRoot.getReductionDims(dims);
       fuseUses = (dims.size() == 1 &&
-                  (linalgRoot.getStaticLoopRanges()[dims[0]] % 64 == 0) &&
+                  (linalgRoot.getStaticLoopRanges()[dims[0]] % (64 * 4) == 0) &&
                   (linalgRoot.getStaticLoopRanges()[dims[0]] <= 4096));
     }
     // Only fuse on inputs if both ops are generic ops.
